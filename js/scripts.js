cc0d1bc8e9b7f2ad12867cb8c5143ddf647494bc
Luis Nabais <dextro@gmail.com>
2011-04-03 17:53:06 +0100

    More design work (timeline is mostly done, post form is about 20% done)

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,38 @@ function Console () {
 }
 console = new Console();
 
+function togglePostForm() {
+    var form = $('post'),
+        oldBottom = $('contents').getChildren('.bottom').getLast();
+    
+    if (form.isDisplayed()) {
+        form.hide();
+        oldBottom.show();
+    } else {
+        oldBottom.hide();
+        form.show();
+    }
+}
+
 window.addEvent('domready', function() {
+    // Detect mac and invert the window controls
+    if (air.Capabilities.os.substring(0,6) == "Mac OS") {
+        window.document.body.addClass("mac");
+    }
+
     $('minimize').addEvent('click', function(e) {
             window.nativeWindow.minimize();
+            e.stop();
+        });
+    
+    $('maximize').addEvent('click', function(e) {
+            if (window.nativeWindow.displayState == "maximized") {
+                window.nativeWindow.restore();
+                window.document.body.removeClass("maximized");
+            } else {
+                window.nativeWindow.maximize();
+                window.document.body.addClass("maximized");
+            }
             e.stop();
         });
 
@@ -26,4 +55,19 @@ window.addEvent('domready', function() {
             window.nativeWindow.startResize();
             e.stop();
         });
+    
+    $$('.post-button').addEvent('click', function(e) {
+            togglePostForm();
+            e.stop();
+        });
+    
+    window.nativeWindow.addEventListener('displayStateChange', function(e) {
+            if (window.nativeWindow.displayState == "maximized") {
+                window.document.body.addClass("maximized");
+            } else {
+                window.document.body.removeClass("maximized");
+            }
+        });
+
+    air.Introspector.Console.log("");
 });
---
+++ libs/mootools/mootools-more-1.3.1.1-nocompat.js
@@ -0,0 +1,114 @@
+// MooTools: the javascript framework.
+// Load this file's selection again by visiting: http://mootools.net/more/d9a9c87e2bba53299aa890efc9f9c8ce 
+// Or build this file again with packager using: packager build More/More More/Element.Shortcuts
+/*
+---
+
+script: More.js
+
+name: More
+
+description: MooTools More
+
+license: MIT-style license
+
+authors:
+  - Guillermo Rauch
+  - Thomas Aylott
+  - Scott Kyle
+  - Arian Stolwijk
+  - Tim Wienk
+  - Christoph Pojer
+  - Aaron Newton
+
+requires:
+  - Core/MooTools
+
+provides: [MooTools.More]
+
+...
+*/
+
+MooTools.More = {
+	'version': '1.3.1.1',
+	'build': '0292a3af1eea242b817fecf9daa127417d10d4ce'
+};
+
+
+/*
+---
+
+script: Element.Shortcuts.js
+
+name: Element.Shortcuts
+
+description: Extends the Element native object to include some shortcut methods.
+
+license: MIT-style license
+
+authors:
+  - Aaron Newton
+
+requires:
+  - Core/Element.Style
+  - /MooTools.More
+
+provides: [Element.Shortcuts]
+
+...
+*/
+
+Element.implement({
+
+	isDisplayed: function(){
+		return this.getStyle('display') != 'none';
+	},
+
+	isVisible: function(){
+		var w = this.offsetWidth,
+			h = this.offsetHeight;
+		return (w == 0 && h == 0) ? false : (w > 0 && h > 0) ? true : this.style.display != 'none';
+	},
+
+	toggle: function(){
+		return this[this.isDisplayed() ? 'hide' : 'show']();
+	},
+
+	hide: function(){
+		var d;
+		try {
+			//IE fails here if the element is not in the dom
+			d = this.getStyle('display');
+		} catch(e){}
+		if (d == 'none') return this;
+		return this.store('element:_originalDisplay', d || '').setStyle('display', 'none');
+	},
+
+	show: function(display){
+		if (!display && this.isDisplayed()) return this;
+		display = display || this.retrieve('element:_originalDisplay') || 'block';
+		return this.setStyle('display', (display == 'none') ? 'block' : display);
+	},
+
+	swapClass: function(remove, add){
+		return this.removeClass(remove).addClass(add);
+	}
+
+});
+
+Document.implement({
+
+	clearSelection: function(){
+		if (window.getSelection){
+			var selection = window.getSelection();
+			if (selection && selection.removeAllRanges) selection.removeAllRanges();
+		} else if (document.selection && document.selection.empty){
+			try {
+				//IE fails here if selected element is not in dom
+				document.selection.empty();
+			} catch(e){}
+		}
+	}
+
+});
+
